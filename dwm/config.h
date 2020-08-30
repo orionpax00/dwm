03394d04d5ffdb6a73bc4c58adc3cdfe5773e52e
Durgesh <durgesh123.iitr@gmail.com>
2020-08-30 23:31:18 +0530

    status patch

--- dwm/config.h
+++ dwm/config.h
@@ -6,6 +6,8 @@ static const unsigned int gappx     = 4;        /* gaps */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "DejaVu Sans Mono:size=10" };
 static const char dmenufont[]       = "DejaVu Sans Mono:size=10";
 static const char col_gray1[]       = "#0d1015";
